27a00f3d8f6e317bb693c34a8ed5e1535f808660
KyleTheHack3r <thepmteam@hotmail.com>
2015-05-12 10:00:13 +0100

    Update hacks.js

--- hacks/hacks.js
+++ hacks/hacks.js
@@ -1,1 +1,27 @@
 // Uploading soon
+var speed = 0;
+var fly = 0;
+var gamemode = 0;
+
+// other stuff here
+
+function  newLevel() {
+  clientMessage("<hacks> All mods reset.");
+  speed = 0;
+  fly = 0;
+}
+
+// UI to enable mods here...
+
+if (speed == 1){
+  clientMessage("<hacks> Speed has been enabled.")
+  ModPE.setGameSpeed(35);
+}
+if (fly == 1){
+  clientMessage("<hacks> Flying has been enabled.")
+  Player.setCanFly(1);
+}
+if (gamemode == 1){
+  clientMessage("<hacks> Gamemode has been enabled.")
+  setGamemode(1)
+}
